56198f1fdb9e552deff3fa893f5d33383aab6848
Computational Biology & Bioinformatics Laboratory <cb2l@users.noreply.github.com>
2016-04-30 09:39:30 +0530

    Initial commit
    CB2l®: Application settings to export in to Build System.

---
+++ config/settings/settings.js
@@ -0,0 +1,9 @@
+/*!
+ * CB2l®      : Apps Settings [+]
+ * ______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ______________________________________________________________
+ */
