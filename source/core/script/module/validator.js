b2debe759a6275d8e500b8416e4756d9a971c6a2
Computational Biology & Bioinformatics Laboratory <cb2l@users.noreply.github.com>
2016-05-02 12:34:01 +0530

    Initial commit
    validator.js, The Core JavaScript - Non-AMD "Form Validations".

---
+++ source/core/script/module/validator.js
@@ -0,0 +1,20 @@
+/*!
+ * CB2L®: Computational Biology and Bioinformatics Laboratory.
+ * _______________________________________________________________
+ * The Core JavaScript - Non-AMD "Form Validations".
+ * _______________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/].
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/].
+ * _______________________________________________________________
+ * @date      : 10-April-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * _______________________________________________________________
+ *
+ * --/The Heart of Non-AMD System/-- of "CB2L®".
+ * _______________________________________________________________
+ */
