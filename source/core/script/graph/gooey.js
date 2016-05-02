5cf58675940860f3b8f77494105c47212f2751f2
Computational Biology & Bioinformatics Laboratory <cb2l@users.noreply.github.com>
2016-05-02 12:42:09 +0530

    Initial commit
    gooey.js, The Non-AMD JavaScript - for defining "Gooey Effect".

---
+++ source/core/script/graph/gooey.js
@@ -0,0 +1,21 @@
+/*!
+ * CB2L®: Computational Biology and Bioinformatics Laboratory.
+ * _______________________________________________________________________
+ * CB2L®, https://github.com/cb2l
+ * The Non-AMD JavaScript - for defining "Gooey Effect".
+ * _______________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * _______________________________________________________________________
+ * @date      : 02-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * _______________________________________________________________________
+ *
+ * --/The Heart of Non-AMD System/-- of "CB2L®".
+ * _______________________________________________________________________
+ */
